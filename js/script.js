b700b12127b33a400743cf4ba2e58022b70fb6ee
oleg <mr.lazy0770@gmail.com>
2023-02-25 11:40:54 +0200

    add sidenav

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var elems1 = document.querySelectorAll('.sidenav');
-  var instances1 = M.Sidenav1.init(elems, {});
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
 });
